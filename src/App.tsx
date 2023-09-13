2eacc911fd4abd5788be77289ace9a1833da031a
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-09-13 20:07:12 +0900

    STYLE: style 글로벌 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 
-import Layout from './styles/Layout.tsx';
 import ChartPage from './pages/ChartPage';
+
+import GlobalStyle from './styles/style.js';
+import styled from 'styled-components';
 
 function App() {
   return (
     <>
-      <Layout />
-      <ChartPage />
+      <GlobalStyle />
+      <Container>
+        <ChartPage />
+      </Container>
     </>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  margin: 40px auto;
+`;
