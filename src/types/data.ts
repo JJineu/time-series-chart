476752e159140bd72ad5ac157fda65838e72d5f1
Joice(Hyunjin Kim) <96639305+JJineu@users.noreply.github.com>
2023-09-13 20:07:56 +0900

    FEAT: type 정의 및 상수화

--- src/types/data.ts
+++ src/types/data.ts
@@ -1,10 +1,6 @@
-type IdataValue = {
+export type Idata = {
+  time: string;
   id: string;
   value_area: number;
   value_bar: number;
-};
-
-export type Idata = {
-  time: string;
-  value: IdataValue;
 };
